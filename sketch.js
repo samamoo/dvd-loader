2586a2958a87247e0ba6732361c595900703dc9f
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2021-02-08 23:18:10 -0800

    Added color to the dvd logo whenever it hits the side

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let y;
 let xspeed;
 let yspeed;
 let dvd;
+let r,g,b;
 
 function preload() {
   dvd = loadImage("dvd-rom-logotype.png")
@@ -12,12 +13,20 @@ function setup() {
   createCanvas(800, 600);
   x = random(width);
   y = random(height);
-  xspeed = 10;
-  yspeed = 10;
+  xspeed = 5;
+  yspeed = 5;
+  pickColour();
+}
+
+function pickColour() {
+  r = random(100, 256);
+  g = random(100, 256);
+  b = random(100, 256);
 }
 
 function draw() {
   background(0);
+  tint(r,g,b);
   dvd.resize(150, 0)
   image(dvd, x, y)
 
@@ -28,15 +37,19 @@ function draw() {
   if (x + dvd.width >= width) {
     xspeed = -xspeed;
     x = width - dvd.width;
+    pickColour();
   } else if ( x <= 0) {
     xspeed = -xspeed;
     x = 0;
+    pickColour();
   }
   if (y + dvd.height >= height) {
     yspeed = -yspeed;
     y = height - dvd.height;
+    pickColour();
   } else if (y <= 0) {
     yspeed = -yspeed;
     y = 0;
+    pickColour();
   }
 }
